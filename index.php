458dd44626fd35ed03ae4a00ac6483b1d22eb4a7
Enzopaspris <enzo.martinet49460@gmail.com>
2025-12-03 11:19:16 +0100

    ajout affiche + boostrap + un peu de css

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DVDtèque</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Ma DVDtèque</h1>
 <?php
 require 'database.php';
 
@@ -6,10 +19,11 @@ $films = $query->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 
-<table>
+<table class="table table-striped">
     <tr>
+        <th>Affiche</th>
         <th>Titre</th>
-        <th>Année de sortie test</th>
+        <th>Année de sortie</th>
         <th>Genres</th>
         <th>Pays</th>
     </tr>
@@ -18,10 +32,14 @@ $films = $query->fetchAll(PDO::FETCH_ASSOC);
 
     if (count($films) > 0) {
         foreach ($films as $row) {
-            echo "<tr><td>" . $row["titre"] . "</td><td>" . $row["annee"] . "</td><td>" . $row["genres"] . "</td><td>" . $row["pays"] . "</td></tr>";
+            echo "<tr><td><img src=" . $row["affiche"] . "/> </td><td>" . $row["titre"] . "</td><td>" . $row["annee"] . "</td><td>" . $row["genres"] . "</td><td>" . $row["pays"] . "</td></tr>";
         }
     } else {
         echo "<tr><td colspan='3'>Aucune donnée</td></tr>";
     }
     ?>
 </table>
+
+</body>
+
+</html>

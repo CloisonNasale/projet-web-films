ee09b4e5b02af0c5ba0cf298556634f9e8841323
Enzopaspris <enzo.martinet49460@gmail.com>
2026-01-08 11:36:24 +0100

    modif css page connexion/inscription

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const Encore = require('@symfony/webpack-encore');
 
+
+
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
@@ -21,6 +23,8 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
+    .addEntry('login', './assets/css/login.css')
+    .addEntry('register', './assets/css/register.css')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -71,6 +75,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-;
+    ;
 
 module.exports = Encore.getWebpackConfig();
